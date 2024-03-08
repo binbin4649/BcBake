441c04afb50725a10fa6171afbb734c73ff7524b
HIDEICHI SAITO <hideichisaito@HIDEICHIs-MacBook-Air.local>
2024-03-08 17:57:37 +0900

    [WIP]ServiceのTestを追加。2nd commit.

--- src/Command/Bake/ServiceTestCommand.php
+++ src/Command/Bake/ServiceTestCommand.php
@@ -12,11 +12,32 @@
 
         public function fileName(string $name): string
         {
-            return $name . 'ServiceTest.php';
+            // return $name . 'ServiceTest.php';
+            return 'tests/TestCase/Service/' . $name . 'ServiceTest.php';
         }
 
         public function template(): string
         {
             return 'Service/service_test';
         }
+
+        /**
+         * templateData
+         * @param Arguments $arguments
+         * @return array
+         */
+        public function templateData(Arguments $arguments): array
+        {
+            $namespace = Configure::read('App.namespace');
+            if ($this->plugin) {
+                $namespace = $this->_pluginNamespace($this->plugin);
+            }
+            $name = $arguments->getArgument('name');
+            return [
+                'namespace' => $namespace,
+                'singularName' => Inflector::singularize($name),
+                'pluralName' => $name
+            ];
+        }
+
     }
